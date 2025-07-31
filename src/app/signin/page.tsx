d4cf2e171d8c7898e2ac46a802d2751d7af7b240
Gnaneshwari Sarla <gnaneshwarisarla001@gmail.com>
2025-07-31 10:16:01 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).     at cr

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -96,7 +96,7 @@ export default function SignInPage() {
                 </Link>
              </div>
           </form>
-        </Content>
+        </CardContent>
       </Card>
     </div>
   )
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCF9tpVp3eDnYJMLpYWGu_b_oDHnhBp76M",
+  authDomain: "savelife-871c3.firebaseapp.com",
+  databaseURL: "https://savelife-871c3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "savelife-871c3",
+  storageBucket: "savelife-871c3.appspot.com",
+  messagingSenderId: "760109239873",
+  appId: "1:760109239873:web:1d74d87cf8556534e8c0c0",
+  measurementId: "G-T37QRFD07W"
 };
 
 // Initialize Firebase
