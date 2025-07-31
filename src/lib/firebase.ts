393e2dacea9c08394a43fd566283f8880e8a9124
Gnaneshwari Sarla <gnaneshwarisarla001@gmail.com>
2025-07-31 09:53:36 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).     at cr

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "savelife-vyo4b",
-  appId: "1:791001388600:web:ca0a75e8ffa1a269a3f8cd",
-  storageBucket: "savelife-vyo4b.firebasestorage.app",
-  apiKey: "AIzaSyAWSDWTh0w8812k3_V2S0N570vF0WnufIM",
-  authDomain: "savelife-vyo4b.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "791001388600",
-  databaseURL: "https://savelife-vyo4b-default-rtdb.firebaseio.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
